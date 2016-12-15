4961cd837641a039476439d722d311b4a8f6700b
Alpers <uskkda03@us.kellogg.com>
2016-12-15 11:48:21 -0500

    Module 9 work in progress

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+// This file isn't transpiled, so you must use CommonJS and ES5
+
+// Register Babel to transpile before tests run
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
+
